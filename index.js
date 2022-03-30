4cfd6de8fb7e75f0161f8f6449911357f33d1d8a
Pranav Pratap Singh <pranavpratapsingh108@gmail.com>
2022-03-30 10:05:30 +0530

    R&W file synchronously

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const fs = require('fs');
 
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-            fs.writeFile('./txt/final.txt', `this contend is being added in this file : \n${data2} \n${data3}`, err => {
-                console.log('File written succesfully🤩');
-            });
-        });
-    });
-});
+
+//Blocking, synchronous way
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+const textOut = `The content of input.txt file is : ${textIn}\n Created on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log("File written!");

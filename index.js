acb5a71df77a20118eebfaba3ab64942c8bdcdf1
Pranav Pratap Singh <pranavpratapsingh108@gmail.com>
2022-03-30 10:55:56 +0530

    Created a simple server

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
+const http = require('http');
 
+////////////////////////////////////////////////////////////
+//File Handeling Intro
 
 //Blocking, synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -10,16 +13,28 @@ const fs = require('fs');
 
 
 //Non-Blocking, Asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err1, data) => {
-    if (err1) return console.log("Unable to read start.txt file");
-    fs.readFile(`./txt/${data}.txt`, 'utf-8', (err2, data2) => {
-        if (err2) return console.log(`Unable to read ${data2}.txt file`);
-        fs.readFile('./txt/append.txt', 'utf-8', (err3, data3) => {
-            if (err3) return console.log(`Unable to read append.txt file`);
-            fs.writeFile('./txt/final.txt', `Content of final file is : ${data2} \n ${data3}`, 'utf-8', (err) => {
-                if (err) throw err;
-                console.log("file written successFully!");
-            });
-        });
-    });
+// fs.readFile('./txt/start.txt', 'utf-8', (err1, data) => {
+//     if (err1) return console.log("Unable to read start.txt file");
+//     fs.readFile(`./txt/${data}.txt`, 'utf-8', (err2, data2) => {
+//         if (err2) return console.log(`Unable to read ${data2}.txt file`);
+//         fs.readFile('./txt/append.txt', 'utf-8', (err3, data3) => {
+//             if (err3) return console.log(`Unable to read append.txt file`);
+//             fs.writeFile('./txt/final.txt', `Content of final file is : ${data2} \n ${data3}`, 'utf-8', (err) => {
+//                 if (err) throw err;
+//                 console.log("file written successFully!");
+//             });
+//         });
+//     });
+// });
+
+///////////////////////////////////////////////////////////////////
+//SERVER
+
+const server = http.createServer((req, res) => {
+    // console.log(req);
+    res.end("Hello from Server!");
+});
+
+server.listen(8800, '127.0.0.2', () => {
+    console.log("listening to request on port 8800");
 });
